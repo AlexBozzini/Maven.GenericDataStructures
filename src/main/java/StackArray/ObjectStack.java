f0b3e7f09c78329082fbad79161899fa9ba7561b
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-25 11:35:14 -0400

    Finished stack array w object

--- src/main/java/StackArray/ObjectStack.java
+++ src/main/java/StackArray/ObjectStack.java
@@ -9,8 +9,31 @@ import java.util.Arrays;
  */
 public class ObjectStack<E> {
     private Object[] elements;
+    private int top;
+    private final static int DEFAULT_SIZE = 10;
 
-    public ObjectStack() {
+    public ObjectStack(){
+        this(DEFAULT_SIZE);
+    }
 
+    public ObjectStack(int initSize) {
+        elements = new Object [initSize];
+        top = -1;
+    }
+
+
+    public Boolean isEmpty(){
+        return top == -1;
+    }
+
+    public void push(E item){
+        elements[++top] = item;
+    }
+
+    public Object pop(){
+        if (isEmpty()){
+            throw new IndexOutOfBoundsException();
+        }
+        return elements[top--];
     }
 }
