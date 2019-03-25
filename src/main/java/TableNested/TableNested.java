af1bcf75dc283e6a5a06c06f4ed4e9120a62509e
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-25 13:47:40 -0400

    finished nested table

--- src/main/java/TableNested/TableNested.java
+++ src/main/java/TableNested/TableNested.java
@@ -7,5 +7,56 @@ import java.util.ArrayList;
  * Think about how nested classes should work with generics.
  */
 public class TableNested<K, V> {
+    private ArrayList<Entry<K,V>> entries;
 
+    public TableNested() {
+        this.entries = new ArrayList<>();
+    }
+
+    public class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+    }
+
+    public V get (K key){
+        for (Entry entry : entries){
+            if (entry.getKey().equals(key)){
+                return (V) entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    public void put(K key, V value){
+        Entry<K,V> entry = new Entry<>(key, value);
+        if (get(key) == null) {
+            entries.add(entry);
+        } else {
+            remove(key);
+            entries.add(entry);
+        }
+    }
+
+    public void remove(K key){
+        for (Entry entry : entries){
+            if (entry.getKey().equals(key)){
+                entries.remove(entry);
+                break;
+            }
+        }
+    }
 }
